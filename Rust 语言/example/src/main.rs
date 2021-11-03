c2d5b63507a539b4bc755df9fc2cce0a5f29b4df
xiayulu <zuiaiqiansen@163.com>
2021-11-03 16:06:16 +0800

    finish the book

--- Rust 语言/example/src/main.rs
+++ Rust 语言/example/src/main.rs
@@ -1,23 +1,4 @@
 fn main() {
-    print!(
-        "{}, {}, {}",
-        (1..9).fold(0, |acc, _| acc + 1),
-        (1..9).fold(0, |acc, x| acc + x),
-        (1..9).fold(1, |acc, x| acc * x),
-    );
+    let result = format!("{} + {} = {}", 600, 66, 600 + 66);
+    println!("{:?}", result); // "600 + 66 = 666"
 }
-
-/*
-#[derive(Debug)]
-struct Point(i32, i32);
-
-impl Point {
-    fn create_point_ref(x: i32, y: i32) -> &Self {
-        &Point(x, y)
-    }
-}
-fn main() {
-    let ref_p = Point::create_point_ref(1, 2);
-    println!("{:?}", ref_p);
-}
-*/
